04fdadd19700a085124ae6a0d7e721ebb7831c57
Артур Кочесоков <artkoches@gmail.com>
2024-09-24 15:36:33 +0300

    # Начало работы. Создан серверу на порту(3003). Установлен 'nodemon' и настроен его запуск через script.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const http = require('http');
+const getUsers = require('./modules/users')
 
 const server = http.createServer((request, response) => {
+    if(request.url==='/users'){
+    response.status = 200
+    response.statusMessage = 'OK'
+    response.header = 'Content-Type: application/json'
+    response.write(getUsers())
+    response.end()
+
+    return
+    }
+
+    response.status = 200
+    response.statusMessage = 'OK'
+    response.header = 'Content-Type: text/plain'
+    response.write('Hello, world!')
+    response.end()
 
     // Написать обработчик запроса:
     // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
@@ -10,3 +26,7 @@ const server = http.createServer((request, response) => {
     // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
 
 });
+
+server.listen(3003, ()=>{
+    console.log('Сервер запущен по адресу http://127.0.0.1:3003/')
+})   

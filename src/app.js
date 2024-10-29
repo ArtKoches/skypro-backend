7fc1372d00d908b5444babd86bc93a77bbf055cb
Артур Кочесоков <artkoches@gmail.com>
2024-10-29 07:59:16 +0300

    Подключена база данных MongoDb. Установлены все зависимости для работы базы. Рефакторинг кода. Ошибок нет.

--- src/app.js
+++ src/app.js
@@ -1,79 +1,29 @@
-// const http = require('http');
-// const getUsers = require('./modules/users')
-// const host = 'http://127.0.0.1'
-// const port = 3003
-        
-// const endpoint = {
-//     MAIN: '/',
-//     USERS: '/?users',
-//     HELLO: '/?hello'
-// }
-
-// const server = http.createServer((request, response) => {
-//     const url = new URL(request.url, host)
-//     const userName = url.searchParams.get('hello')
-
-//     if(userName) {
-//         response.statusCode = 200
-//         response.statusMessage = 'OK'
-//         response.appendHeader("Content-Type", "text/plain")
-//         response.write(`Hello, ${userName}.`)   
-//         response.end()
-
-//         return
-//     }
-    
-//     switch (request.url) {
-//         case endpoint.MAIN:
-//             response.statusCode = 200
-//             response.statusMessage = 'OK'
-//             response.appendHeader("Content-Type", "text/plain")
-//             response.write('Hello, World!')
-//             response.end()
-//             break;
-//         case endpoint.USERS:
-//             response.statusCode = 200
-//             response.statusMessage = 'OK'
-//             response.appendHeader("Content-Type", "application/json")
-//             response.write(getUsers())
-//             response.end()
-//             break;
-//         case endpoint.HELLO:
-//             response.statusCode = 400
-//             response.statusMessage = 'Bad Request'
-//             response.appendHeader("Content-Type", "text/plain")
-//             response.write('Enter a name')
-//             response.end()
-//             break;
-    
-//         default:
-//             response.statusCode = 500
-//             response.statusMessage = 'Server Error'
-//             response.end()    
-//             break;
-//     }
-// });
-
-// server.listen(port, () => {
-//     console.log(`Сервер запущен по адресу ${host}:${port}/`)    
-// })
-
-//START HW-04
 const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const userRouter = require('./routes/users')
 const loggerOne = require('./middlewares/loggerOne')
 
 dotenv.config()
 
-const app = express()
-
 const {
     PORT = 3000,
-    API_URL = 'http://127.0.0.1'
+    API_URL = 'http://127.0.0.1',
+    MONGO_URL = 'mongodb://localhost:27017/mydb'
 } = process.env
+
+mongoose.connect(MONGO_URL)
+.then(()=>{
+    console.log('Connected to MongoDb')
+})
+.catch((err)=> {
+    if(err) throw err
+    console.log('MongoDb Error')
+}) 
+
+const app = express()
 
 const helloWorld = (request, response) => {
     response.status(200)
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,26 +1,61 @@
+const User = require('../models/user')
+
 const getUsers = (request, response) => {
 	// Get all users
+	return User.find({})
+	.then((data) => {
+		response.status(200).send(data)
+	})
+	.catch((err) => {
+		response.status(500).send(err.message)
+	})
 }
 
 const getUser = (request, response) => {
 	// Get user
 	const { user_id } = request.params
-	response.status(200)
-	response.send(`User with id: ${user_id}`)
+	return User.findById(user_id)
+	.then((user) => {
+		response.status(200).send(user)
+	})
+	.catch((err) => {
+		response.status(500).send(err.message)
+	})
 }
 
 const createUser = (request, response) => {
 	// Create new user
-	response.status(201)
-	response.send(request.body)
+	return User.create({ ...request.body })
+	.then((user) => {
+		response.status(201).send(user)
+	})
+	.catch((err) => {
+		response.status(500).send(err.message)
+	})
 }
 
 const updateUser = (request, response) => {
 	// Update user
+	const { user_id } = request.params
+	return User.findByIdAndUpdate(user_id, { ...request.body })
+	.then((user) => {
+		response.status(200).send(user)
+	})
+	.catch((err) => {
+		response.status(500).send(err.message)
+	})
 }
 
 const deleteUser = (request, response) => {
 	// Delete user
+	const { user_id } = request.params
+	return User.findByIdAndDelete(user_id)
+	.then(() => {
+		response.status(200).send('User success deleted')
+	})
+	.catch((err) => {
+		response.status(500).send(err.message)
+	})
 }
 
 module.exports = {
